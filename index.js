77b7830aa6dece269efeb240dcfde1c375211c8b
nguyenphuongduy124 <nguyenphuongduy124@gmail.com>
2020-07-02 16:04:04 +0700

    Install Pug & demo form search

--- index.js
+++ index.js
@@ -2,9 +2,42 @@ var express = require('express');
 var app = express();
 var port = 3000;
 
-app.get('/', function(req, res) {
-    res.send('Hello CoderX');
+// set template engine
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+
+var users = [{
+    name: 'Duy',
+}, {
+    name: 'Thuy',
+}];
+
+// get
+app.get('', function(req, res) {
+    res.render('index', {
+        name: "CodersX"
+    });
 })
+
+app.get('/users', function(req, res) {
+    res.render('users/index', {
+        users: users
+    });
+})
+
+app.get('/users/search', function(req, res) {
+    console.log(req.query);
+    var q = req.query.q;
+    var matchesUsers = users.filter(function(user) {
+        return user.name.toLowerCase().indexOf(q.toLowerCase()) > -1;
+    })
+    res.render('users/index', {
+        users: matchesUsers,
+        qSearch: q
+    })
+})
+
 
 app.listen(port, function() {
     console.log('Server running on port: ', port);
